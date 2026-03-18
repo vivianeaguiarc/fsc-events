0fae87cf75983cadd67ac67ee970ad4d4d235a53
Viviane Aguiar <vivianeaguiarc@outlook.com>
2026-03-18 11:15:29 -0300

    chore: add eslint setup

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+]);
--- index.ts
+++
@@ -1,7 +0,0 @@
-const sumAsync = async (a: number, b: number) => {
-  return a + b
-}
-
-await sumAsync(2, 3).then(result => console.log(result))
-
-export { sumAsync }
